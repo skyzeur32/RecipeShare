215773ea33feed356647a8970706312163d864ff
Sami <sroudji32@gmail.com>
2021-12-26 15:45:50 +0100

    Premier push

---
+++ server/recipeshare/src/main/java/com/recipeshare/recipeshare/RecipeshareApplication.java
@@ -0,0 +1,13 @@
+package com.recipeshare.recipeshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeshareApplication.class, args);
+	}
+
+}
---
+++ server/recipeshare/src/test/java/com/recipeshare/recipeshare/RecipeshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipeshare.recipeshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
